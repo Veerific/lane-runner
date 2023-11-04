b993c74dae9b4b7c703c9631777b5d089e97c566
VLaptop2 <majarov@hva.nl>
2023-11-04 22:58:58 +0100

    player can jump

--- lanerunner/Assets/Scripts/PlayerMovement.cs
+++ lanerunner/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameObject laneLeft, laneMiddle, laneRight;
     private int currentPlayerPos;
+    private bool isJumping;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float jumpVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
                 transform.position = new(laneRight.transform.position.x, transform.position.y, transform.position.z);
                 break;
         }
+
+        if(isJumping) { 
+            rb.AddForce(Vector3.up * jumpVelocity);
+            isJumping = false;
+        }
     }
 
     // Update is called once per frame
@@ -38,15 +46,15 @@ public class PlayerMovement : MonoBehaviour
     void Movement()
     {
         if (Input.GetKeyDown(KeyCode.A)){
-            print("aa");
-            print(currentPlayerPos);
+          
             currentPlayerPos = currentPlayerPos > 0 ? currentPlayerPos-=1: 0;
         }
 
         if (Input.GetKeyDown(KeyCode.D)){
-            print("aaa");
-            print(currentPlayerPos);
+            
             currentPlayerPos = currentPlayerPos < 2 ? currentPlayerPos+=1 : 2;
         }
+        if(Input.GetKeyDown(KeyCode.W)) isJumping = true;
+        
     }
 }
